c422059578de393684cb9c502cd80481658c0224
Gorkem Baris Yesiltas <gorkemyesiltas@hotmail.com>
2024-11-13 17:50:30 +0100

    adding native forms

---
+++ components/Native/AddressForm.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+    <form method="post" action="/api/address">
+        <label for="name">Full name</label>
+        <input type="text" name="name" />
+
+        <label for="email">Email</label>
+        <input type="email" name="email" />
+
+        <label for="phone">Phone</label>
+        <input type="tel" name="phone" />
+
+        <label for="streetName">Street name</label>
+        <input type="text" name="streetName" />
+
+        <label for="city">City</label>
+        <input type="text" name="city" />
+
+        <CountryInput />
+
+        <label for="isDefault">Mark as default address</label>
+        <input type="checkbox" name="isDefault" />
+
+        <input type="submit" value="Submit">
+    </form>
+</template>
+
+<style scoped lang="postcss">
+input {
+    @apply border p-2;
+}
+</style>
---
+++ components/Native/CountryInput.vue
@@ -0,0 +1,7 @@
+<template>
+    <label for="country">Country</label>
+    <select name="country">
+        <option value="nl">Netherlands</option>
+        <option value="gb">United Kingdom</option>
+    </select>
+</template>
