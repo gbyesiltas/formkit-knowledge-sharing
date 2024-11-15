8403157e68fd30c466b72fc87c46c6f8f617eb0f
Gorkem Baris Yesiltas <gorkemyesiltas@hotmail.com>
2024-11-15 15:59:59 +0100

    added address form

--- app.vue
+++ app.vue
@@ -4,5 +4,6 @@
 <template>
   <div class="max-w-5xl mx-auto p-16">
     <h1 class="text-3xl font-bold">Web Forms</h1>
+    <AddressForm />
   </div>
 </template>
---
+++ components/AddressForm.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- 
+        - type
+        - name and id
+        - placeholder
+        - help
+    -->
+    <FormKit type="form">
+        <FormKit type="text" name="name" label="Name" placeholder="Enter your full name" />
+        <FormKit type="email" name="email" label="Email" help="We won't send you emails" />
+        <FormKit type="select" name="country" :options="[{ label: 'Netherlands', value: 'nl' }, { label: 'United Kingdom', value: 'uk' }]" />
+    </FormKit>
+</template>
+
+<style lang="postcss">
+input {
+    @apply border p-2;
+}
+</style>
